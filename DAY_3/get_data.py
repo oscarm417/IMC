292073f76f2f5fc5e9a151ea56095fb65a143144
jaJann312 <jannik.proff.hamburg@gmail.com>
2023-03-24 15:37:10 +0100

    Data cleanup and new version for day 3

---
+++ DAY_3/get_data.py
@@ -0,0 +1,19 @@
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+
+class Trader:
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        """
+        Only method required. It takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+        
+        result = {}
+        
+        for product in state.order_depths.keys():
+            
+            print(state.observations)
+
+        return result
